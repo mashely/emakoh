76f0c65cee8a6feca35b76b96787a1082b61faea
mashely <ansherotimalnado10@gmail.com>
2023-12-28 13:49:33 +0300

    update copyright and owner

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Fp Kidijitali.
+                <script>document.write(new Date().getFullYear())</script> © Emakoh Digital.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design , Developed and Maintained by Fp Kidijitali
+                    Design , Developed and Maintained by Emakoh Digital
                 </div>
             </div>
         </div>
