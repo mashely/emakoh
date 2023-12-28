d5d2dcddfa2d195824b3b471992c974f3f9d61af
mashely <ansherotimalnado10@gmail.com>
2023-12-28 13:50:55 +0300

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 <head>
         
         <meta charset="utf-8" />
-        <title>Fp Kidijitali</title>
+        <title>edigital</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Digital Hospital" name="Digital Reminder" />
         <meta content="Hospital" name="Bluetick Technology" />
