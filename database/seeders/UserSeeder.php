556851f5e2a62b0d8f0274d2fea9bc2ede399cb6
deogemini <geminideogratias8@gmail.com>
2023-12-30 08:36:59 +0300

    working on some corrections of migrations

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +17,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        
+         // Generate user data here
+         $users = [
+            [
+                'email'=>'admin@gmail.com',
+                'password' => Hash::make('password'),
+            ]
+            ];
+             // Insert users into the 'users' table
+        DB::table('users')->insert($users);
     }
 }
