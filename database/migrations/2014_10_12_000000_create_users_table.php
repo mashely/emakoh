9f5dedb12421c751a19f4e9cc7246e38025254ca
deogemini <geminideogratias8@gmail.com>
2024-01-05 21:09:30 +0300

    add missing column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->tinyInteger('active')->nullable();
             $table->string('username')->nullable();
             $table->unsignedBigInteger('created_by')->nullable(); // Foreign key to users table
+            $table->unsignedBigInteger('updated_by')->nullable(); // Foreign key to users table
             $table->rememberToken();
             $table->timestamps();
         });
